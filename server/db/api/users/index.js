46af8902225b92b43923b18dd52b12834db8c503
nick <nicnhines3@gmail.com>
2018-03-01 13:57:53 -1000

    user and item routes

---
+++ server/db/api/users/index.js
@@ -0,0 +1,53 @@
+const express = require(`express`);
+const router = express.Router();
+const User = require('../../models/User')
+
+module.exports = router;
+
+// router
+//   .route('/')
+//   .get((req, res) => {
+//     console.log('user route smoke test')
+//   })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    return new User({id: req.params.id})
+      .fetch()
+      .then((user) => {
+        return res.json(user);
+      })
+      .catch((err) => {
+        return res.status(400).json({
+          message: err.message
+        });
+      });
+  })
+  .put((req, res) => {
+    return new User({id: req.params.id})
+      .fetch()
+      .save(req.body, {require: true})
+      .then((user) => {
+        return res.json(user)
+      })
+      .catch((err) => {
+        return res.status(400).json({
+          message: err.message
+        });
+      });
+  });
+
+// router
+//   .route('/login')
+//   .post((req, res) => {
+//     let {user, password} = req.body.username;
+//     return new User({'username': user, 'password': password})
+//       .fetch()
+//       .then((loggedIn) => {
+//         return res.json(loggedIn)
+//       })
+//       .catch((err) => {
+//         message : err.message
+//       })
+//   })
